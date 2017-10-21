82ace97bd73e53ede434aab4eb269b497d63bd57
Kris Hermstad <krishermstad@gmail.com>
2017-10-21 02:59:42 -0400

    fully functional, needs refactoring

--- src/main/scala/S3PGHelper.scala
+++ src/main/scala/S3PGHelper.scala
@@ -16,7 +16,8 @@ object S3PGHelper {
     val s3Bucket = ConfigFactory.load().getString("s3Bucket")
 
     val awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey)
-    val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard.withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build
+    val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard.withCredentials(
+      new AWSStaticCredentialsProvider(awsCreds)).build
 
 
     implicit val s3 = S3.at(Region.US_EAST_1)
@@ -44,13 +45,14 @@ object S3PGHelper {
     }
 
     // filtered to only keys that end with .mp3
-    val mp3keys = keys.filter(isMp3);
+    val keysFromS3 = keys.filter(isMp3);
 
     // now look for each key in db, if not there, delete it in s3
     val connectionUrl = ConfigFactory.load().getString("postgresUrl")
-    println(connectionUrl)
 
-    class Tracks(tag: Tag) extends Table[(String, String, String, String, String, Int, Boolean, String, String,  Int)](tag, "tracks"){
+    class Tracks(tag: Tag) extends Table[(String, String, String, String, String,
+      Int, Boolean, String, String, Int)](tag, "tracks") {
+
       def s3key = column[String]("s3key", O.PrimaryKey)
       def email = column[String]("email")
       def bucket = column[String]("bucket")
@@ -61,17 +63,33 @@ object S3PGHelper {
       def created = column[String]("created")
       def rejected = column[String]("rejected")
       def trackid = column[Int]("trackid")
+
       def * = (s3key, email, bucket, title, description, downloads, approved, created, rejected, trackid)
     }
 
-    Database.forURL(connectionUrl, driver= "org.postgresql.Driver") withSession {
+    var keysFromPostgres = ListBuffer[String]();
+
+    Database.forURL(connectionUrl, driver = "org.postgresql.Driver") withSession {
       implicit session =>
         val keys = TableQuery[Tracks]
 
         keys.list foreach { row =>
-          println(row)
+          keysFromPostgres += row._1
         }
 
+    }
+
+    println(keysFromS3)
+    println(keysFromPostgres)
+
+    // subtract keys from db with s3 - remove remainder from s3
+    val keydiff = keysFromS3 -- keysFromPostgres
+
+    println(keydiff.size)
+
+    // iterate through keys and remove from s3
+    for (i <- 0 until keydiff.size) {
+      s3Client.deleteObject(s3Bucket, keydiff(i))
     }
 
   }
