e66e0efb62867c2e0ae22f969264059b48dc3435
Kris Hermstad <krishermstad@gmail.com>
2017-10-21 13:01:17 -0400

    refactored and commented file

--- src/main/scala/S3PGHelper.scala
+++ src/main/scala/S3PGHelper.scala
@@ -11,6 +11,7 @@ object S3PGHelper {
 
   def main(args: Array[String]): Unit = {
 
+    // load aws config
     val awsAccessKey = ConfigFactory.load().getString("awsAccessKey")
     val awsSecretKey = ConfigFactory.load().getString("awsSecretKey")
     val s3Bucket = ConfigFactory.load().getString("s3Bucket")
@@ -19,8 +20,7 @@ object S3PGHelper {
     val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion("us-east-1").withCredentials(
       new AWSStaticCredentialsProvider(awsCreds)).build
 
-
-
+    // get bucket
     var bucketList = s3Client.listObjects(s3Bucket)
     var objectSummaries = bucketList.getObjectSummaries
     while (bucketList.isTruncated()) {
@@ -30,11 +30,13 @@ object S3PGHelper {
 
     var keys = ListBuffer[String]()
 
+    // get all keys
     for (i <- 0 until objectSummaries.size()) {
       keys += objectSummaries.get(i).getKey
 
     }
 
+    // returns true if key ends in .mp3
     def isMp3(key: String): Boolean = {
       if (key.length < 5) return false
       if (key.substring(key.length - 4, key.length) == ".mp3") {
@@ -49,6 +51,7 @@ object S3PGHelper {
     // now look for each key in db, if not there, delete it in s3
     val connectionUrl = ConfigFactory.load().getString("postgresUrl")
 
+    // Tracks model for PG db
     class Tracks(tag: Tag) extends Table[(String, String, String, String, String,
       Int, Boolean, String, String, Int)](tag, "tracks") {
 
@@ -68,29 +71,25 @@ object S3PGHelper {
 
     var keysFromPostgres = ListBuffer[String]();
 
+    // db connection
     Database.forURL(connectionUrl, driver = "org.postgresql.Driver") withSession {
       implicit session =>
         val keys = TableQuery[Tracks]
 
+        // iterate through tracks, push s3key to keysFromPostgres
         keys.list foreach { row =>
           keysFromPostgres += row._1
         }
 
     }
 
-    println(keysFromS3)
-    println(keysFromPostgres)
-
     // subtract keys from db with s3 - remove remainder from s3
     val keydiff = keysFromS3 -- keysFromPostgres
-
-    println(keydiff.size)
 
     // iterate through keys and remove from s3
     for (i <- 0 until keydiff.size) {
       s3Client.deleteObject(s3Bucket, keydiff(i))
     }
-
   }
 }
 
